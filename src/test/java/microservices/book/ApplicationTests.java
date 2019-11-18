2d716949183bad707d8e69dfd270bc52c8ca735d
Brahim <d.yassine@gmail.com>
2019-11-18 13:28:36 +0100

    initial commit service1 is done

---
+++ src/test/java/microservices/book/ApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.book;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
